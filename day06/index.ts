36bec75392cdfab7512654b1986ab88a159fe575
cstp <759168442@qq.com>
2021-09-28 15:10:08 +0800

    add typescript

--- day06/index.ts
+++ day06/index.ts
@@ -341,3 +341,317 @@
 // let c = new Child();
 
 // console.log("我是子类:" + c.apiUrl)
+
+//1. 复习昨天的讲的内容
+
+//2. 在ts中如何定义类
+
+    //通过class关键词定义类
+
+
+
+//3. 在类中如何定义属性 和 方法 
+
+    // class App {
+    //     // name : string = "hello"
+    //     name:string;
+
+    //     //实例化类的时候会触发constructor方法
+    //     constructor(name:string){
+    //         this.name = "123"
+    //     }
+
+    //     //定义原型上的方法
+    //     run(){
+
+    //     }
+    // }
+
+//4. 如何调用类里面的属性和方法
+
+
+    //对类进行实例化,通过实例化之后的对象进行调用
+    // class App{
+        // name:string;
+        // constructor(){
+        //     this.name= "jack"
+        // };
+        // run():void{
+        //     alert("123")
+        // }
+    // }
+
+    // let app = new App();
+    // app.run()
+    // console.log(app.name)
+
+//5. ts中通过类实现继承 
+
+    // 核心方法: extends关键词实现继承
+
+    //父类
+    // class App {
+    //     name:string;
+    //     constructor(){
+    //         this.name= "jack"
+    //     };
+    //     run():void{
+    //         alert("123")
+    //     }
+    // }
+    // //子类
+    // class User extends App{
+
+    // }
+
+    // let u = new User();
+
+    // console.log(u.name)
+    // u.run()
+
+
+//6. 如何实例化子类时讲将参数传递给父类
+
+    // 核心方法: super方法
+
+    //父类
+    // class App{
+    //     name:string;
+    //     age : number;
+
+    //     constructor(name:string, age:number){
+    //         this.name = name;
+    //         this.age = age;
+    //     }
+
+    //     getUser():string {
+    //         return this.name + this.age;
+    //     }
+    // }
+
+    // //子类
+
+    // class User extends App{
+        
+    //     constructor(name:string, age:number){
+    //         super(name, age);
+    //     }
+    // }
+
+    // let u = new User("jack",20);
+
+
+//7. ts中的修饰符
+
+    // 设置定义属性可访问的权限或者范围
+
+    // public 类的内部 子类 类的外部
+    //protected  类的内部和子类 不能在外部访问
+    //private 当前类的内部 不能在子类和外部访问
+
+
+
+
+// function User(){
+
+//     this.name = "rose";
+//     this.run = function(){
+
+//     }
+
+// }
+
+// User.name = "jack";
+// User.run = function(){
+//     alert("hello world")
+// }
+
+
+
+// let u = new User();
+
+// u.name 
+// u.run
+
+// User.name
+// User.run
+
+//ts中定义静态属性和方法
+// class User{
+//     name:string;
+//     constructor(name:string){
+//         this.name = name;
+//     }
+//     run(){
+//         alert(this.name)
+//     }
+// }
+
+// let u = new User("hello");
+// u.name
+// u.run
+
+// class User{
+//     name:string;
+
+//     //定义静态属性
+//     static age:number = 100;
+
+//     //定义静态方法
+//     static getTest():number{
+//         return User.age + 100;
+//     }
+
+//     constructor(name:string){
+//         this.name = name;
+//     }
+//     run(){
+//         alert(this.name)
+//     }
+// }
+
+// let u = new User("hello");
+
+// console.log(User.age)
+// console.log(User.getTest())
+
+//抽象类
+abstract class User{
+    name:string;
+    abstract age:number;
+    constructor(){
+        this.name = "123"
+    }
+    abstract test():void;
+
+    run(){
+        alert("hello wrold")
+    }
+}
+
+class App extends User{
+    age = 123;
+    test(){
+        console.log("hello world")
+    }
+}
+
+let app = new App();
+
+
+//多态:父类定义一个方法不去实现，让继承它的子类去实现  每一个子类有不同的表现    
+
+//多态属于继承
+        /*
+
+
+                class Animal {
+
+                    name:string;
+                    constructor(name:string) {
+                        this.name=name;
+                    }
+                    eat(){   //具体吃什么  不知道   ，  具体吃什么?继承它的子类去实现 ，每一个子类的表现不一样
+                        console.log('吃的方法')
+                    }
+                }
+
+                class Dog extends Animal{
+                    constructor(name:string){
+                        super(name)
+                    }
+                    eat(){
+                
+                        return this.name+'吃粮食'
+                    }
+                }
+
+
+                class Cat extends Animal{
+
+                    constructor(name:string){
+                        super(name)
+                    }
+
+                    eat(){
+
+                        return this.name+'吃老鼠'
+                    }
+                }
+
+        */
+
+
+
+//typescript中的抽象类：它是提供其他类继承的  基(基础) 类  ，不能直接被实例化。
+
+
+//用abstract关键字定义抽象类和抽象方法，抽象类中的抽象方法不包含具体实现并且必须在派生类中实现。
+
+
+// abstract抽象方法只能放在抽象类里面
+
+
+// 抽象类和抽象方法用来定义标准 。   标准：Animal 这个类要求它的子类必须包含eat方法
+
+
+//标准:
+
+abstract class Animal{
+    
+    public name:string;
+    constructor(name:string){
+
+        this.name=name;
+
+    }
+    abstract eat():any;  //抽象方法不包含具体实现并且必须在派生类中实现。
+    
+    run(){
+
+        console.log('其他方法可以不实现')
+    }
+}
+
+
+// var a=new Animal() /*错误的写法*/
+ 
+
+
+class Dog extends Animal{
+
+    //抽象类的子类必须实现抽象类里面的抽象方法
+    constructor(name:any){
+        super(name)
+    }
+    eat(){
+
+        console.log(this.name+'吃粮食')
+    }
+}
+
+var d=new Dog('小花花');
+d.eat();
+
+
+
+
+class Cat extends Animal{
+
+    //抽象类的子类必须实现抽象类里面的抽象方法
+    constructor(name:any){
+        super(name)
+    }
+    run(){
+
+
+    }
+    eat(){
+
+        console.log(this.name+'吃老鼠')
+    }
+    
+}
+
+var c=new Cat('小花猫');
+c.eat();
